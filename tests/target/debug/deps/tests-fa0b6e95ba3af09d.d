707c0d8438b2d1d188df85b937abde7de18ef2d5
Camila <camilaayala01@gmail.com>
2023-03-26 18:43:13 -0300

    faltan chequeos

---
+++ tests/target/debug/deps/tests-fa0b6e95ba3af09d.d
@@ -0,0 +1,5 @@
+/home/camila/Documents/taller/ej_ind/ajedrez/tests/target/debug/deps/tests-fa0b6e95ba3af09d: src/lib.rs
+
+/home/camila/Documents/taller/ej_ind/ajedrez/tests/target/debug/deps/tests-fa0b6e95ba3af09d.d: src/lib.rs
+
+src/lib.rs:
